f200257875b7303676afa8dc29df787ebd414c69
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-01-23 16:23:59 -0800

    Collatz Algorithm

--- collatzalgorithm.py
+++ collatzalgorithm.py
@@ -3,8 +3,9 @@ number =int(input("Enter a Number: "))
 while number != 1:
     if number%2 ==1:
         number = (number *3) + 1
-        print(number)
+        
     else:
         number = number//2
-        print(number)
+        
+    print(number)
     
