061d417f2dbfa4e06f71ee2b329ea939d719b2af
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-01-23 16:03:02 -0800

    check input for perfect number

---
+++ perfectnumber.py
@@ -0,0 +1,21 @@
+number = int(input ("Enter a number: "))
+factor = 1
+sum = 0
+while factor < number:
+    if number%factor==0:
+       sum += factor
+       
+    factor +=1
+
+
+if sum == number:
+    print("This number is a perfect number!")
+elif sum < number:
+    print("Oohs! This is a deficient number")
+        
+elif sum > number:
+    print("arrgh! This is an abundant number")
+
+else:
+    print("Goodbye")
+        
