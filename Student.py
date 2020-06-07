1b8354c8712258fd053f4868857a644cb4535f44
Fatunbi David Oluwakayode <fatunbidavidkayode@gmail.com>
2020-06-06 18:37:51 -0700

    First python class. Experimenting...

--- Student.py
+++ Student.py
@@ -15,3 +15,20 @@ class Student(object):
     def __str__(self):
         return "{}  {}, ID: {}". \
             format(self.first_name_str, self.last_name_str, self.student_id_int)
+
+
+def main ():
+    student1 = Student("James", "Bond", 189)
+    student2 = Student("Anthony", "Joshua")
+    print(student1)
+    print(student2)
+    print('Updated Record')
+    print('='*20)
+    student1.update(student_id=134)
+    print(student1)
+    student2.update(student_id=189)
+    print(student2)
+
+
+main()
+
